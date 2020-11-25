774b53ab996cbae3912a2622a9786a931c68f12f
braleo <73036769+braleo@users.noreply.github.com>
2020-11-25 12:34:48 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var bullet,wall;
+var speed,weight,thickness;
+
+
+function setup() {
+  createCanvas(1600,400);
+ bullet= createSprite(50, 200, 50, 50);
+ thickness = random(22,83);
+ wall= createSprite(1300,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);
+  speed=random(223,321);
+  weight = random(30,52);
+  
+
+  bullet.velocityX=speed;
+}
+
+function draw() {
+  background('purple');  
+  if(hasCollided(bullet,wall)){
+bullet.velocityX=0;
+var damage =0.5 * weight*speed*speed/(thickness*thickness*thickness);
+
+if(damage>10){
+
+  wall.shapeColor=color(255,0,0);
+}
+if(damage<10){
+
+  wall.shapeColor=color(0,255,0);
+}
+  }
+
+
+  drawSprites();
+}
+
+
+
+function hasCollided(bullet,wall)
+{
+bulletRightEdge=bullet.x + bullet.width;
+wallLeftEdge=wall.x;
+if(bulletRightEdge>=wallLeftEdge){
+  return true
+}
+ else {
+  return false
+}
+
+}
+
